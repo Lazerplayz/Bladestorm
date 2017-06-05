9101fce549519b4868ec399717fa49f21618d5f9
Keith <keith@imagicalmine.net>
2017-06-05 16:27:20 +1000

    Fix egg spawning

--- src/pocketmine/entity/Egg.php
+++ src/pocketmine/entity/Egg.php
@@ -62,7 +62,7 @@ class Egg extends Projectile{
 	public function spawnTo(Player $player){
 		$pk = new AddEntityPacket();
 		$pk->type = Egg::NETWORK_ID;
-		$pk->eid = $this->getId();
+		$pk->entityRuntimeId = $this->getId();
 		$pk->x = $this->x;
 		$pk->y = $this->y;
 		$pk->z = $this->z;
